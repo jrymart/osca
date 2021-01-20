6b2ace14ec05eb0a433e292fdec2e923644dfc33
Jo Martin <jo@moosecow.net>
2021-01-19 21:18:15 -0500

    mostly notes on the lottery

---
+++ src/lottery/lottery.js
@@ -0,0 +1,17 @@
+// variable names
+var IDKey = "entryID"
+var isPairKey = "isConsecutivePair"
+var 
+function getLotteryEntries() {
+	// to be replaced with db call
+	return [{entryID: 1234, name: "Ivanna Pee"},
+					 {entryID: 5678, name: "Miss Direction"}, 
+					 {entryID: 90210, name: "Corey Dreamcicle"}
+				  ];
+};
+
+function getLotteryOrder(lotteryEntries) {
+	// handles consecutive pairs
+};
+
+module.exports = { getLotteryEntries };
