976a2ac3d96738d0b3e485be83c99a372dfebb5b
Jo Martin <jo@moosecow.net>
2021-01-24 17:55:31 -0500

    more notes and a key lottery function

--- src/lottery/lottery.js
+++ src/lottery/lottery.js
@@ -1,6 +1,9 @@
 // variable names
 var IDKey = "entryID"
 var isPairKey = "isConsecutivePair"
+var coopKey = "coop"
+var diningCapacityKey = "diningCapacity"
+var housingCapacityKey = "housingCapacity"
 var 
 function getLotteryEntries() {
 	// to be replaced with db call
@@ -13,5 +16,19 @@ function getLotteryEntries() {
 function getLotteryOrder(lotteryEntries) {
 	// handles consecutive pairs
 };
+
+function generateCoopSpace(coop) {
+	var space_entry = {coopKey:coop[coopKey], housingCapacityKey: coop[housingCapacityKey]};
+}
+
+function getAssignments(lotteryEntries, lotteryConstraints) {
+	lotteryEntries.forEach(entry => {
+		entry[preferencesKey].forEach(function (preference, index) {
+			if coopSpaces[preference][freeSpaceKey] > 0 {
+				coopSpaces[preference][freSpaceKey]--
+				entry[assignmentKey] = preference
+				break		
+		})
+	})
 
 module.exports = { getLotteryEntries };
